3ba09c64d23d8649c8e74655e26460c058df6933
GMgsz <1668017453@qq.com>
2022-03-09 08:52:43 +0800

    Comment仓储

--- WUSTZone/Domain/Repositories/CommentRepositoryDB.cs
+++ WUSTZone/Domain/Repositories/CommentRepositoryDB.cs
@@ -1,6 +1,50 @@
-﻿namespace WUSTZone.Domain.Repositories
+﻿using System.Collections.Generic;
+using System.Linq;
+using WUSTZone.Domain.Entityies;
+
+namespace WUSTZone.Domain.Repositories
 {
-    public class CommentRepositoryDB:ICommentRepository
+    public class CommentRepositoryDB : ICommentRepository
     {
+        private readonly AppDbContext context;
+
+        // 依赖注入
+        public CommentRepositoryDB(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Comment Add(Comment comment)
+        {
+            context.Comments.Add(comment);
+            context.SaveChanges();
+            return comment;
+        }
+
+        public bool Delete(int id)
+        {
+            Comment comment = context.Comments.Find(id);
+            if (comment != null)
+            {
+                context.Comments.Remove(comment);
+                return true;
+            }
+            return false;
+        }
+        public List<Comment> GetByPostId(int id)
+        {
+            return context.Comments
+                    .Where(Comment => Comment.Id == id && Comment.FatherId == 0)
+                    .ToList();
+        }
+
+        public List<Comment> GetByFatherId(int id)
+        {
+            return context.Comments
+                      .Where(Comment => Comment.FatherId == id)
+                      .ToList();
+        }
+
+        
     }
 }
--- WUSTZone/Domain/Repositories/ICommentRepository.cs
+++ WUSTZone/Domain/Repositories/ICommentRepository.cs
@@ -1,9 +1,39 @@
-﻿using WUSTZone.Domain.Entityies;
+﻿using System.Collections.Generic;
+using WUSTZone.Domain.Entityies;
 
 namespace WUSTZone.Domain.Repositories
 {
     public interface ICommentRepository
     {
-       
+        /// <summary>
+        /// 增加一条评论
+        /// </summary>
+        /// <param name="comment"></param>
+        /// <returns>Comment实体</returns>
+       public Comment Add(Comment comment);
+
+        /// <summary>
+        /// 删除一条评论
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>bool</returns>
+       public bool Delete(int id);
+
+
+        /// <summary>
+        /// 获取留言下的所有一级评论
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public List<Comment> GetByPostId(int id);
+
+
+        /// <summary>
+        /// 获取一级评论下的所有留言
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public List<Comment> GetByFatherId(int id);
+
     }
 }
